ce5da40fb0f1cc1278880d80ed0b541fde0ea947
React <react_programmer@Edgars-Air.lan1>
2020-01-06 21:24:46 -0600

    React Week 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*
+This file is not a module due it does not have export statement
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App"; /*Calling the App module form App.js file in the same folder*/
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "typeface-lobster";
+import "typeface-open-sans";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
