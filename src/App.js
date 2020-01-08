287d1b94d360fe7477c27cd2022e31a4918fffdd
React <react_programmer@Edgars-Air.lan1>
2020-01-08 01:03:53 -0600

    React Component Types [VI]

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
-import Directory from "./components/DirectoryComponent";
-import { CAMPSITES } from "./shared/campsites";
 import Main from "./components/MainComponent";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      campsites: CAMPSITES
-    };
-  }
   render() {
     return (
       <div className="App">
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,22 +1,21 @@
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Directory from "./DirectoryComponent";
-import CampsiteInfo from "./CampsiteInfoComponent";
 import { CAMPSITES } from "../shared/campsites";
+import CampsiteInfo from './CampsiteInfoComponents'
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      campsites: CAMPSITES,
+      campsites: CAMPSITES
       selectedCampsite: null
     };
   }
 
-  onCampsiteSelect(campsiteId) {
-    this.setState({ selectedCampsite: campsiteId });
+  onCampsiteSelect(campsite){
+      this.setState({selectedCampsite:campsiteID})
   }
-
   render() {
     return (
       <div>
@@ -25,20 +24,12 @@ class Main extends Component {
             <NavbarBrand href="/">NuCamp</NavbarBrand>
           </div>
         </Navbar>
-        <Directory
-          campsites={this.state.campsites}
-          onClick={campsiteId => this.onCampsiteSelect(campsiteId)}
-        />
-        <CampsiteInfo
-          campsite={
-            this.state.campsites.filter(
-              campsite => campsite.id === this.state.selectedCampsite
-            )[0]
-          }
-        />
+        <Directory campsites={this.state.campsites} onClick={campsiteID => this.onCampsiteSelect(campsiteID)} />
+        <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/>
       </div>
     );
   }
 }
 
+/*A JavaScript moculde is a JavaScript file that contains at least one export.*/
 export default Main;
