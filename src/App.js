f324c794a680607c4177c6f59705c64ccfbdf1f8
React <react_programmer@Edgars-Air.lan1>
2020-01-09 22:00:29 -0600

    New Approach

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from "react";
 import "./App.css";
 import Main from "./components/MainComponent";
@@ -10,7 +11,31 @@ class App extends Component {
       </div>
     );
   }
+=======
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+>>>>>>> parent of ce5da40... React Week 2
 }
 
-/*A JavaScript moculde is a JavaScript file that contains at least one export.*/
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-/*
-This file is not a module due it does not have export statement
-*/
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App"; /*Calling the App module form App.js file in the same folder*/
-import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "typeface-lobster";
-import "typeface-open-sans";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
