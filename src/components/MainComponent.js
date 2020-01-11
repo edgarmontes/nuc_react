0abb3d6657bbaa6648193cfee7b59e8e8a73868f
React <react_programmer@Edgars-Air.lan1>
2020-01-11 08:08:23 -0600

    Presentational and Container Components

---
+++ src/components/MainComponent.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Directory from "./DirectoryComponent";
+import { CAMPSITES } from "../shared/campsites";
+import CampsiteInfo from "./CampsiteInfoComponents";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+      selectedCampsite: null
+    };
+  }
+
+  onCampsiteSelect(campsiteID) {
+    this.setState({ selectedCampsite: campsiteID });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">NuCamp</NavbarBrand>
+          </div>
+        </Navbar>
+        <Directory
+          campsites={this.state.campsites}
+          onClick={campsiteID => this.onCampsiteSelect(campsiteID)}
+        />
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              campsite => campsite.id === this.state.selectedCampsite
+            )[0]
+          }
+        ></CampsiteInfo>
+      </div>
+    );
+  }
+}
+
+/*Copied from Apps.js*/
+/*This is a new branch 1920*/
+export default Main;
