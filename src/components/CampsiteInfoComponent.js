5d461818d6da9287bb688ec7797d434b5ec8cf34
React <react_programmer@Edgars-Air.lan1>
2020-01-22 23:58:51 -0600

    Redux Thunk I

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -18,6 +18,7 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from "./LoadingComponent";
 
 const maxLength = len => val => !val || val.length <= len;
 const minLength = len => val => val && val.length >= len;
